73e751ffde53d9e42c079683d6abd5ad8cfb4f6b
Maxime <maxime.darcherif@gmail.com>
2025-03-18 15:05:58 +0100

    implémentation du config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    """Configuration de base"""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-very-secret'
+    DEBUG = True
+    
+   # (mettre ses infos si elles sont différentes)
+    DB_USERNAME = os.environ.get('DB_USERNAME') or 'root'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or ''
+    DB_HOST = os.environ.get('DB_HOST') or 'localhost'
+    DB_NAME = os.environ.get('DB_NAME') or 'smart_home'
+    
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SESSION_TYPE = 'filesystem'
